bddefee2dd92c20cf7ee9f6c519af1c9425a1bba
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 15:01:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,8 +14,8 @@ def map_to_square(source_array)
   source_array.map {|e| e*e}
 end
 
-def reduce_to_total(source_array, starting_point)
-  source_array.reduce(starting_point, :+) 
+def reduce_to_total(source_array, starting_point=0)
+  source_array.reduce(starting_point) {|sum, num| sum + num} 
 end
 
 def reduce_to_all_true(source_array)
